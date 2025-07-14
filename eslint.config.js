47170cf58e9daed47af8b8ad43f6f5ec1cd53e75
Anonymous4078 <95581967+Anonymous4078@users.noreply.github.com>
2025-07-14 11:26:36 +0530

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    files: ['**/*.{ts}'],
+  },
+  {
+    ignores: ['node_modules/'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './src/tsconfig.json',
+      },
+    },
+  },
+  {
+    rules: {
+      // eslint
+      'dot-notation': 'error',
+      eqeqeq: 'error',
+      'no-caller': 'error',
+      'no-constant-condition': ['error', { checkLoops: false }],
+      'no-eval': 'error',
+      'no-extra-bind': 'error',
+      'no-new-func': 'error',
+      'no-new-wrappers': 'error',
+      'no-return-await': 'error',
+      'no-template-curly-in-string': 'error',
+      'no-throw-literal': 'error',
+      'no-undef-init': 'error',
+      'no-var': 'error',
+      'object-shorthand': 'error',
+      'prefer-const': 'error',
+      'prefer-object-spread': 'error',
+      'unicode-bom': ['error', 'never'],
+
+      // @typescript-eslint/eslint-plugin
+      /* '@typescript-eslint/restrict-template-expressions': 'off',
+      '@typescript-eslint/non-nullable-type-assertion-style': 'off',
+      '@typescript-eslint/no-unnecessary-condition': 'off',
+      '@typescript-eslint/no-deprecated': 'off', */
+    },
+  },
+);
