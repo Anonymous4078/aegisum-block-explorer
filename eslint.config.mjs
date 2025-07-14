686865bdb10d789a7582e12b0cb92f7737ebb6ac
Anonymous4078 <95581967+Anonymous4078@users.noreply.github.com>
2025-07-14 13:00:27 +0530

    Update and rename eslint.config.js to eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import next from 'eslint-config-next';
 
 export default tseslint.config(
   {
@@ -11,6 +12,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
+  next,
   {
     languageOptions: {
       parserOptions: {
