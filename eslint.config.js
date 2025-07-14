35d87760a4f1c75d288b5d0850ba12c76bab662d
Anonymous4078 <95581967+Anonymous4078@users.noreply.github.com>
2025-07-14 18:05:56 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,49 @@
-import pluginNext from '@next/eslint-plugin-next';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
   {
-    plugins: { '@next/next': pluginNext },
     files: ['**/*.{ts,tsx,js,jsx}'],
-    rules: {
-      ...pluginNext.configs.recommended.rules,
-      // optionally include core-web-vitals:
-      // ...pluginNext.configs['core-web-vitals'].rules,
+  },
+  {
+    ignores: ['node_modules/'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+      },
     },
   },
-  tseslint.config(
-    { files: ['**/*.{ts,tsx}'] },
-    { ignores: ['node_modules/'] },
-    eslint.configs.recommended,
-    ...tseslint.configs.strictTypeChecked,
-    ...tseslint.configs.stylisticTypeChecked,
-    {
-      languageOptions: {
-        parserOptions: { project: './tsconfig.json' },
-      },
+  {
+    rules: {
+      // eslint
+      'dot-notation': 'error',
+      eqeqeq: 'error',
+      'no-caller': 'error',
+      'no-constant-condition': ['error', { checkLoops: false }],
+      'no-eval': 'error',
+      'no-extra-bind': 'error',
+      'no-new-func': 'error',
+      'no-new-wrappers': 'error',
+      'no-return-await': 'error',
+      'no-template-curly-in-string': 'error',
+      'no-throw-literal': 'error',
+      'no-undef-init': 'error',
+      'no-var': 'error',
+      'object-shorthand': 'error',
+      'prefer-const': 'error',
+      'prefer-object-spread': 'error',
+      'unicode-bom': ['error', 'never'],
+
+      // @typescript-eslint/eslint-plugin
+      /* '@typescript-eslint/restrict-template-expressions': 'off',
+      '@typescript-eslint/non-nullable-type-assertion-style': 'off',
+      '@typescript-eslint/no-unnecessary-condition': 'off',
+      '@typescript-eslint/no-deprecated': 'off', */
     },
-    {
-      rules: {
-        'dot-notation': 'error',
-        eqeqeq: 'error',
-        'no-var': 'error',
-        // … other custom rules …
-      },
-    }
-  ),
-];
+  },
+);
