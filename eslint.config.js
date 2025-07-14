b11fde77a6439a0e9459d48354e482e15a224e0c
Anonymous4078 <95581967+Anonymous4078@users.noreply.github.com>
2025-07-14 13:26:23 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,9 @@ import { FlatCompat } from '@eslint/eslintrc';
 const compat = new FlatCompat({ baseDirectory: import.meta.url });
 
 export default [
-  ...compat.extends('next/core-web-vitals'),
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript']
+  }),
   tseslint.config(
   {
     files: ['**/*.{ts}'],
