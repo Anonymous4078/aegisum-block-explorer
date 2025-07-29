ddf19ecca0365d8f7518b195637c318a5db03619
Anonymous4078 <95581967+Anonymous4078@users.noreply.github.com>
2025-07-29 19:37:15 +0530

    Update alert-dialog.tsx

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import * as React from "react";
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
+import * as React from "react";
 
-import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 const AlertDialog = AlertDialogPrimitive.Root;
 
@@ -32,7 +32,7 @@ const AlertDialogContent = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Content>
 >(({ className, ...props }, ref) => (
   <AlertDialogPortal>
-    <AlertDialogOverlay />
+    <AlertDialogOverlay/>
     <AlertDialogPrimitive.Content
       ref={ref}
       className={cn(
